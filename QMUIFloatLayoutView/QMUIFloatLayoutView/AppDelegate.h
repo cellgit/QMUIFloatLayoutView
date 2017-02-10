09f0f6ad17de41e5ad71d793e7193d7f1d59f7d2
liuhongli <hongli@shoplex.com>
2017-02-10 12:03:34 +0800

    创建项目

---
+++ QMUIFloatLayoutView/QMUIFloatLayoutView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QMUIFloatLayoutView
+//
+//  Created by iosdeveloper on 2017/2/10.
+//  Copyright © 2017年 liuhongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QMUIFloatLayoutView/QMUIFloatLayoutView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QMUIFloatLayoutView
+//
+//  Created by iosdeveloper on 2017/2/10.
+//  Copyright © 2017年 liuhongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
