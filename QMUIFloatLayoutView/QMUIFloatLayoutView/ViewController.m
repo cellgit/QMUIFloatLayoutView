590bc97c56ab146f5da894d5fa1681a307933cf5
liuhongli <hongli@shoplex.com>
2017-02-10 12:37:02 +0800

    添加pod文件以及Modules文件

--- QMUIFloatLayoutView/QMUIFloatLayoutView/ViewController.m
+++ QMUIFloatLayoutView/QMUIFloatLayoutView/ViewController.m
@@ -16,14 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"QMUIFloatLayoutView";
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
+
+
 
 
 @end
